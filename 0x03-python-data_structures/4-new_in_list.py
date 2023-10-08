abde96ddea2cf21fe20b65828952bfb5b93ca000
TRINIXEI <oladejiifeoluwaeunice@gmail.com>
2023-10-08 14:49:20 -0700

    4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+def new_in_list(my_list, idx, element):
+
+    """
+    # Write a function that replaces an element of
+    # a list at a specific position (like in C).
+    # VARIABLE(" "):
+    # my_list(list) Replace element
+    # If idx is negative, the function should not modify anything,
+    #           and returns the original list
+    # If idx is out of range (> of number of element in my_list),
+    #           the function should not modify anything, and returns the
+    """
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+
+    my_list_2 = my_list[:]
+    my_list_2[idx] = element
+    return my_list_2
