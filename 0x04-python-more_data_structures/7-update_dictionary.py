e80aa57f4ae4b140e0d1246211b175160e2ee9d1
TRINIXEI <oladejiifeoluwaeunice@gmail.com>
2023-10-13 07:22:59 -0700

    7-update_dictionary.py

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    """Replacing or adding key/value pairs in dictionary."""
+    a_dictionary[key] = value
+    return (a_dictionary)
