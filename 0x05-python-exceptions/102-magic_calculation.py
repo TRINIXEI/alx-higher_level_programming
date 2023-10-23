a944b79822cfdf392181be2c72d1550da0f5392e
TRINIXEi <oladejiifeoluwaeunice@gmial.com>
2023-10-23 13:24:48 -0700

    102-magic_calculation.py

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def magic_calculationna, m):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                result += n ** m / i
+        except Exception:
+            result = m + n
+            break
+    return result
