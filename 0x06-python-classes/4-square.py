89a3ee3290aeb8f85f1f34d26fa1ee40eaaf5959
TRINIXEi <oladejiifeoluwaeunice@gmial.com>
2023-10-25 14:45:29 -0700

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Square Class defined with private instance size"""
+
+
+class Square:
+    """Square class.
+    size: the private instance of the class
+    area: is a method of the class Square
+    """
+
+    def __init__(self, size=0):
+        """__init__
+        initialize a private instance.
+        validate size type and value of size
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Get Size Value"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set Size Value"""
+        if not (isinstance(value, int)):
+            raise TypeError("size must be an integer")
+        elif (value < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Calculate the square of size"""
+        return self.__size ** 2
