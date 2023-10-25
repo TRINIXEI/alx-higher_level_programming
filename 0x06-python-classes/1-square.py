3b89d8986f8d81c0cc7cf067ad0d0927fbed2729
TRINIXEi <oladejiifeoluwaeunice@gmial.com>
2023-10-25 14:40:25 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Square class created with attribute"""
+
+
+class Square:
+    """class Square.
+    size: an attribute of the square class
+    """
+    def __init__(self, size):
+        """define a private instance"""
+
+        self.__size = size
